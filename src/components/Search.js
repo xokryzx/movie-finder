4abe0a9ba6ed83ac337243f335aab8a92f2a02d8
xokryzx <xokryzx@gmail.com>
2023-09-05 23:55:35 +0900

    ✨ Add a Movie route to display movie details

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,11 @@ export default class Search extends Component {
   render() {
     this.element.classList.add("search");
     this.element.innerHTML = `
-      <input placeholder="Enter the movie title to search!" class="search-input" />
+      <input
+        value="${movieStore.state.searchText}"
+        placeholder="Enter the movie title to search!"
+        class="search-input"
+      />
       <button class="button button-primary search-button">Search!</button>
     `;
 
---
+++ src/routes/Movie.js
@@ -0,0 +1,53 @@
+import Component from "../core/component";
+import movieStore, { getMovieDetails } from "../store/movie";
+
+export default class Movie extends Component {
+  async render() {
+    await getMovieDetails(history.state.id);
+    const { movie } = movieStore.state;
+    const bigPoster = movie.Poster.replace("SX300", "SX700");
+
+    this.element.classList.add("movie-container");
+    this.element.innerHTML = `
+      <div
+        style = "background-image: url(${bigPoster})"
+        class="poster"
+      ></div>
+      <div class="desc">
+        <div class="title">${movie.Title}</div>
+        <div class="labels">
+          <span>${movie.Released}</span>
+          &nbsp;/&nbsp;
+          <span>${movie.Runtime}</span>
+          &nbsp;/&nbsp;
+          <span>${movie.Country}</span>
+        </div>
+        <div class="plot">${movie.Plot}</div>
+        <div class="detail">
+          <div class="detail-title">Ratings</div>
+          ${movie.Ratings.map((rating) => {
+            return `
+              <p class="detail-content">${rating.Source} - ${rating.Value}</p>
+            `;
+          }).join("")}
+        </div>
+        <div class="detail">
+          <div class="detail-title">Actors</div>
+          <p class="detail-content">${movie.Actors}</p>
+        </div>
+        <div class="detail">
+          <div class="detail-title">Directors</div>
+          <p class="detail-content">${movie.Directors}</p>
+        </div>
+        <div class="detail">
+          <div class="detail-title">Production</div>
+          <p class="detail-content">${movie.Production}</p>
+        </div>
+        <div class="detail">
+          <div class="detail-title">Genre</div>
+          <p class="detail-content">${movie.Genre}</p>
+        </div>
+      </div>
+    `;
+  }
+}
